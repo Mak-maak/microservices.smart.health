ec6b6119720b6b881845c7385ee6e01dc697706c
Mak-maak <azizk7693@gmail.com>
2026-02-26 20:13:20 +0500

    Restore H2-backed context test setup

--- src/audit.api/src/test/java/com/smarthealth/audit/AuditApplicationTests.java
+++ src/audit.api/src/test/java/com/smarthealth/audit/AuditApplicationTests.java
@@ -6,12 +6,12 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
         "spring.flyway.enabled=false",
         "azure.servicebus.connection-string="
 })
